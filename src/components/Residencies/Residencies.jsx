7b3c4fa2fad322254082baaa7e9797a238d9cef8
Langer Pereira <langerpereira12@gmail.com>
2024-09-13 20:58:33 +0530

    slider js

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Residencies.css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import data from "../../utils/slider.json";
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="padding innerWidth r-container"></div>
+    </section>
+  );
+};
+
+export default Residencies;

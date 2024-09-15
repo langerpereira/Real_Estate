8d0e6cb8e81031cc180ad8f6e361f68a830c8d55
Langer Pereira <langerpereira12@gmail.com>
2024-09-15 17:51:21 +0530

    contact, footer,  FINAL COMMIT

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Companies.css";
-
 const Companies = () => {
   return (
     <section className="c-wrapper">
-      <div className="padding innerWidth flexCenter c-container">
+      <div className="paddings innerWidth flexCenter c-container">
         <img src="./prologis.png" alt="" />
         <img src="./tower.png" alt="" />
         <img src="./equinix.png" alt="" />
